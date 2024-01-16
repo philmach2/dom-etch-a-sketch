f856eb965da94a85ad63fd91995c5a0b33d28958
Phil Mach <110493891+philmach2@users.noreply.github.com>
2024-01-16 18:54:56 -0500

    Made clear comments in JS for next steps

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function makeGrid(size) {
 }
 makeGrid(48)
 
-// Need to create hover effect to change the color of the etch a sketch squares to draw
-// background-color: rgb(51, 51, 51) for hover effect
-// add two white knobs w click events to increase or decrease size of grid (16,32,48,64,100)
+// Need to create hover effect (hover event???) to change the color of the etch a sketch squares to draw
+    // background-color: rgb(51, 51, 51) for hover effect
+// Add two white knobs w click events to increase or decrease size of grid (16,32,48,64,100)
+    // default is 48, Click 'LESS' decrease to 32 -> 16 \ Click 'MORE' increase to 64 -> 100

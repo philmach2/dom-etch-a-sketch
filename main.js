fa40cabda27dccf966c5c46c4c25e053297fc912
Phil Mach <110493891+philmach2@users.noreply.github.com>
2024-01-18 11:01:45 -0500

    changed mouseTest variable and mouse event function name

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
-const more = document.querySelector('#more')
-const less = document.querySelector('#less')
-let mouseTest;
+const more = document.querySelector('#more');
+const less = document.querySelector('#less');
+const gridSizes = [8, 16, 32];
+const max = 2;
+const min = 0;
+let currentGridSize = 1;
 
-const gridSizes = [8, 16, 32, 48, 64]
-const max = 4
-const min = 0
-let currentGridSize = 2
-
-more.addEventListener('click', increaseGrid)
-less.addEventListener('click', decreaseGrid)
+more.addEventListener('click', increaseGrid);
+less.addEventListener('click', decreaseGrid);
 
 // Create grid
 function makeGrid(size) {
 
-    const screen = document.querySelector('#sketch-screen')
-    screen.innerHTML = ''
+    const screen = document.querySelector('#sketch-screen');
+    screen.innerHTML = '';
 
     for(let i = 0; i <= size; i++) {
-        const column = document.createElement('div')
+        const column = document.createElement('div');
 
         for(let j = 1; j <= size; j++) {
-            const row = document.createElement('div')
-            row.classList.add('hover')
-            row.style.border = '0.1px dotted rgba(0, 0, 0, 0.2)'
-            column.appendChild(row)
-        }
+            const row = document.createElement('div');
+            row.classList.add('hover');
+            row.style.border = '0.1px dotted rgba(0, 0, 0, 0.2)';
+            column.appendChild(row);
+        };
 
-        screen.appendChild(column)
-    }
-// This is inside so that it exists on each grid creation
-    mouseTest = document.querySelectorAll('div.hover')
+        screen.appendChild(column);
+    };
 
-    Array.from(mouseTest).forEach((element) => {
-        element.addEventListener('mouseenter', testMouseEnter)
-    })
-}
-makeGrid(gridSizes[currentGridSize])
+// This is inside so that it exists on every grid creation
+    const drawScreen = document.querySelectorAll('div.hover');
+
+    Array.from(drawScreen).forEach((element) => {
+        element.addEventListener('mouseenter', draw);
+    });
+};
+
+// Call the function to create the grid
+makeGrid(gridSizes[currentGridSize]);
 
 // Increase grid size
 function increaseGrid() {
 
     if(currentGridSize < max) {
-        currentGridSize++
-        makeGrid(gridSizes[currentGridSize])
-        console.log('Grid size increased.')
+        currentGridSize++;
+        makeGrid(gridSizes[currentGridSize]);
 
     }else {
-        alert(`Oops! Looks like you've reached the maximum screen size. Feel free to draw or make the screen smaller.`)
+        alert(`Oops! Looks like you've reached the maximum screen size. Feel free to draw or make the screen smaller.`);
     }
 }
 
@@ -54,31 +54,21 @@ function increaseGrid() {
 function decreaseGrid() {
 
     if(currentGridSize > min) {
-        currentGridSize--
-        makeGrid(gridSizes[currentGridSize])
-        console.log('Grid size decreased.')
+        currentGridSize--;
+        makeGrid(gridSizes[currentGridSize]);
 
     }else {
-        alert(`Oops! Looks like you've reached the minimum screen size. Feel free to draw or make the screen larger.`)
-    }
-}
-// **This says it's undefined
+        alert(`Oops! Looks like you've reached the minimum screen size. Feel free to draw or make the screen larger.`);
+    };
+};
 
-// function testMouseEnter() {
-//     mouseTest.forEach((element) => {
-//         element.target.style.backgroundColor = 'white'
-//     })
-// }
+function draw(event) {
 
-// **This targets whole columns or rows
-function testMouseEnter(event) {
     const targetElement = event.target;
     
     // Check if the target element has the 'hover' class
     if (targetElement.classList.contains('hover')) {
-        targetElement.style.backgroundColor = 'white';
-    }
-}
+        targetElement.style.backgroundColor = 'rgb(101, 101, 101)';
+    };
 
-// Need to create hover effect (hover event???) to change the color of the etch a sketch squares to draw
-    // background-color: rgb(51, 51, 51) for hover effect
+};
